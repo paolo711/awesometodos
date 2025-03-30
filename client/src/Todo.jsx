db50ca4e5f686db86db901a95a04491fd856720a
paolo <paolojohnlatorilla711@gmai.com>
2025-03-30 21:12:34 +0800

    first commit

---
+++ client/src/Todo.jsx
@@ -0,0 +1,64 @@
+export default function Todo(props) {
+  
+    const updateTodo = async (todoId, todoStatus) => {
+      const updatedStatus = !todoStatus;  
+  
+    
+      const res = await fetch(`/api/todos/${todoId}`, {
+        method: "PUT",
+        body: JSON.stringify({ status: updatedStatus }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const json = await res.json();
+  
+      if (json.acknowledged) {
+       
+        props.setTodos((currentTodos) => {
+          return currentTodos.map((currentTodo) => {
+            if (currentTodo._id === todoId) {
+              return { ...currentTodo, status: updatedStatus };
+            }
+            return currentTodo;
+          });
+        });
+      }
+    };
+  
+
+    const deleteTodo = async (todoId) => {
+      const res = await fetch(`/api/todos/${todoId}`, {
+        method: "DELETE",
+      });
+      const json = await res.json();
+  
+      if (json.acknowledged) {
+       
+        props.setTodos((currentTodos) =>
+          currentTodos.filter((todo) => todo._id !== todoId)
+        );
+      }
+    };
+  
+    return (
+      <div className="todo">
+        <p>{props.todo.todo}</p>
+        <div className="mutations">
+          <button
+            className="todo__status"
+            onClick={() => updateTodo(props.todo._id, props.todo.status)} 
+          >
+            {props.todo.status ? "☑" : "☐"}  
+          </button>
+          <button
+            className="todo__delete"
+            onClick={() => deleteTodo(props.todo._id)}
+          >
+            🗑️
+          </button>
+        </div>
+      </div>
+    );
+  }
+  
